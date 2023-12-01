19a7fb2c6da9aa0281c2c50389faf9dde1727ed0
DDevil NightRaid <ddevil@gmail.com>
2023-12-01 22:10:17 +0545

    day3

---
+++ day3/index.js
@@ -0,0 +1,24 @@
+
+//declaration name () {}
+function printName(name, age){
+    console.log(name);
+    console.log(age);
+}
+
+
+// function call 
+printName(12,'Hari')
+
+// Function Types 
+
+// Arrow function 
+const nicenice = () => {
+    console.log('hi');
+}
+
+nicenice()
+// Anonymous Arrow function:
+// () => {
+//     console.log('hi');
+// }
+
