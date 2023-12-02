9e050fe1bd021f1f9c15d4b25d0cdbacd56af634
DDevil NightRaid <ddevil@gmail.com>
2023-12-02 21:54:55 +0545

    day 4

---
+++ day4/index.js
@@ -0,0 +1,24 @@
+// Loops
+console.log('working')
+// for loop 
+
+let students = ['🚸', '🕶️', '🌸', '🐧', '🐒', '👧'];
+
+// console.log(students.length);
+
+// Shreya almost died again 💀
+// Because of 😈
+
+for(let index = 0; index < students.length; index++){
+    console.log(students[index]);
+}
+
+
+// While loop 
+x = 0
+while (x < students.length) {
+    console.log(students[x]);
+    x++;
+}
+
+// console.log('While loop ended')
